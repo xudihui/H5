52dd94ef693a8976d9b0ac33bc9d93f46911bf3f
xudihui <xudihui@xudihui.com>
2017-08-16 10:49:10 +0800

    https://activity.96225.com/exthtml/unionV3/first.html update modify in props in window# Please enter the commit message for your changes. Lines starting

--- module/wkwebview控制返回刷新/backRefresh.js
+++ module/wkwebview控制返回刷新/backRefresh.js
@@ -6,7 +6,7 @@
     })
     //页面从缓存回到当前视图；页面第一次进入
     window.addEventListener('pageshow', function( e ){
-        if(isPageHide) { 
+        if(window.isPageHide) { 
             window.location.reload(); 
         }    
     })

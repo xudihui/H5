8c65a7701dbcb9b4ec74196909ae4df5c8fcffc3
xudihui <xudihui@xudihui.com>
2018-03-08 17:39:20 +0800

    add new feature

--- module/jsBridge/jsBridge.js
+++ module/jsBridge/jsBridge.js
@@ -7,7 +7,111 @@
 	  GOV:'https://activity.96225.com/exthtml/mobile_links/govMatter/index.html'//政务网生产接口
   };
   
- 
+ var _Base64 = function(){  
+   
+    // private property  
+    var _keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+   
+    // public method for encoding  
+    this.encode = function (input) {  
+        var output = "";  
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;  
+        var i = 0;  
+       // input = _utf8_encode(input);  
+        while (i < input.length) {  
+            chr1 = input.charCodeAt(i++);  
+            chr2 = input.charCodeAt(i++);  
+            chr3 = input.charCodeAt(i++);  
+            enc1 = chr1 >> 2;  
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);  
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);  
+            enc4 = chr3 & 63;  
+            if (isNaN(chr2)) {  
+                enc3 = enc4 = 64;  
+            } else if (isNaN(chr3)) {  
+                enc4 = 64;  
+            }  
+            output = output +  
+            _keyStr.charAt(enc1) + _keyStr.charAt(enc2) +  
+            _keyStr.charAt(enc3) + _keyStr.charAt(enc4);  
+        }  
+        return output;  
+    }  
+   
+    // public method for decoding  
+    this.decode = function (input) {  
+        var output = "";  
+        var chr1, chr2, chr3;  
+        var enc1, enc2, enc3, enc4;  
+        var i = 0;  
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");  
+        while (i < input.length) {  
+            enc1 = _keyStr.indexOf(input.charAt(i++));  
+            enc2 = _keyStr.indexOf(input.charAt(i++));  
+            enc3 = _keyStr.indexOf(input.charAt(i++));  
+            enc4 = _keyStr.indexOf(input.charAt(i++));  
+            chr1 = (enc1 << 2) | (enc2 >> 4);  
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);  
+            chr3 = ((enc3 & 3) << 6) | enc4;  
+            output = output + String.fromCharCode(chr1);  
+            if (enc3 != 64) {  
+                output = output + String.fromCharCode(chr2);  
+            }  
+            if (enc4 != 64) {  
+                output = output + String.fromCharCode(chr3);  
+            }  
+        }  
+        output = _utf8_decode(output);  
+        return output;  
+    }  
+   
+    // private method for UTF-8 encoding  
+   var _utf8_encode = function (string) {
+        string = string.replace(/\r\n/g,"\n");  
+        var utftext = "";  
+        for (var n = 0; n < string.length; n++) {  
+            var c = string.charCodeAt(n);  
+            if (c < 128) {  
+                utftext += String.fromCharCode(c);  
+            } else if((c > 127) && (c < 2048)) {  
+                utftext += String.fromCharCode((c >> 6) | 192);  
+                utftext += String.fromCharCode((c & 63) | 128);  
+            } else {  
+                utftext += String.fromCharCode((c >> 12) | 224);  
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);  
+                utftext += String.fromCharCode((c & 63) | 128);  
+            }  
+   
+        }  
+        return utftext;  
+    }  
+   
+    // private method for UTF-8 decoding  
+   var _utf8_decode = function (utftext) {
+        var string = "";  
+        var i = 0;  
+        var c = c1 = c2 = 0;  
+        while ( i < utftext.length ) {  
+            c = utftext.charCodeAt(i);  
+            if (c < 128) {  
+                string += String.fromCharCode(c);  
+                i++;  
+            } else if((c > 191) && (c < 224)) {  
+                c2 = utftext.charCodeAt(i+1);  
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));  
+                i += 2;  
+            } else {  
+                c2 = utftext.charCodeAt(i+1);  
+                c3 = utftext.charCodeAt(i+2);  
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));  
+                i += 3;  
+            }  
+        }  
+        return string;  
+    }  
+}  
+
+  var Base64 = new _Base64();
   var md5 = (function () {
   
   function safeAdd (x, y) {
@@ -696,6 +800,48 @@
 				}
 				J.getFrame(CONFIG.GOV + '?' + formatParams(data))
 	}
+	
+			
+		/**
+		 * 发起SDK支付
+		 *
+		 * @param {opt} 订单完整字段
+		 * @returns
+		 */
+		var sdkPay = function(e){
+			//{"orderNo":null,"dateTime":null,"amount":0,"cardnumber":null,"goods":null,"merCode":null,"mersign":null,"mertxtypeid":null,"storeid":null,"orderDesc":null,"payTime":null}
+			var r = {
+				appid: "appid",
+				url: "smkapp://smkpayview",
+				params: {
+					orderNo: e.orderNo,
+					dateTime: e.dateTime,
+					amount: "" + e.amount,
+					cardnumber: e.cardnumber,
+					goods: e.goods,
+					merCode: e.merCode,
+					mersign: e.mersign,
+					mertxtypeid: e.mertxtypeid,
+					storeid: e.storeid
+				}
+			};
+			location.href = "smkapp://smkpay/" + Base64.encoder(JSON.stringify(r));
+		}
+
+		/**
+		 * 发起wap支付
+		 *
+		 * @param {t,mode} 订单加密字段
+		 * @returns
+		 */
+		var wapPay = function(t,mode) {
+			if(mode == 'dev'){
+				location.href = "http://115.236.162.166:18081/exthtml/smkpay-h5/index.html#!/externalWapPay.html?smk_pay=" + t //测试环境
+			}
+			else{
+				location.href = "https://mobilepay.96225.com/exthtml/smkpay-h5/index.html#!/externalWapPay.html?smk_pay=" + t //生产环境
+			}
+		}
 
     return {
       issmkApp: issmkApp,
@@ -721,7 +867,9 @@
       protocol: protocol,
       Callbacks: Callbacks,
 	  initAdvertise: adv(),
-	  govMatter:govMatter
+	  govMatter:govMatter,
+	  sdkPay:sdkPay,
+	  wapPay:wapPay
     }
   }
   window.jsBridge = SmkApp(J.init)
